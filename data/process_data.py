a7de6b577116e662d7e007a550fd029d302faec0
Ibrahim <ibrahim.y.hakami@gmail.com>
2019-12-28 16:08:06 +0300

    update to process.py

--- data/process_data.py
+++ data/process_data.py
@@ -62,7 +62,7 @@ def save_data(df, database_filename):
 
     '''
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql(database_filename[:-3], engine, index=False )
+    df.to_sql(database_filename[5:-3], engine, index=False  , if_exists='replace')
     pass  
 
 
